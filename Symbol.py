a5fbf0445315f0639ba264e12ddee6edd7fd384b
LloydTTG <randyngui08@gmail.com>
2026-03-04 11:01:34 +0800

    ORB test

--- Symbol.py
+++ Symbol.py
@@ -6,17 +6,24 @@ cam = Camera(resolution=(640,480), fps=60)
 
 qr = cv2.imread('symbols/qr.png')
 qr = cv2.cvtColor(qr, cv2.COLOR_BGR2GRAY)
+orb = cv2.ORB_create()
+tK, tD = orb.detectAndCompute(qr, None) # tK for template keypoints and d for descriptor
+matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
 while True:
 
     frame = cam.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    orb = cv2.ORB_create()
 
-    tK, tD = orb.detectAndCompute(qr, None) # tK for template keypoints and d for descriptor
+    
     vK, vD = orb.detectAndCompute(frame, None)
 
-    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    if tD is None or vD is None or len(tD) == 0 or len(vD) == 0:
+        cam.display(frame)
+        if cv2.waitKey(1) == 27:
+            break
+        continue
+    
     matches = matcher.match(tD, vD)
 
     frame2 = cv2.drawMatches(qr, tK, frame, vK, matches[:20], None)
