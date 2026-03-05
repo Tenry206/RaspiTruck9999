0e47673faee5ed55f24c96cdab105d89cd064dbd
LloydTTG <randyngui08@gmail.com>
2026-03-05 11:12:19 +0800

    changing orb thresholds

--- Main.py
+++ Main.py
@@ -131,7 +131,7 @@ lost_counter = 0
 search_speed = 0.7
 counter = 0
 
-orb = cv2.ORB_create(nfeatures=1500, fastThreshold=25)
+orb = cv2.ORB_create(nfeatures=1800, fastThreshold=30)
 matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
 templatesF = build_templatesF(templates, orb)
 
