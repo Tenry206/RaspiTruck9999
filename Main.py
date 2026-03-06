29a54dc4b0270647e393e5e89a3c298c215599cb
LloydTTG <randyngui08@gmail.com>
2026-03-06 08:51:49 +0800

    tuned ORB parameters

--- Main.py
+++ Main.py
@@ -131,7 +131,7 @@ lost_counter = 0
 search_speed = 0.7
 counter = 0
 
-orb = cv2.ORB_create(nfeatures=1500, fastThreshold=25, nlevels=12, scaleFactor=1.2, patchSize=21)
+orb = cv2.ORB_create(nfeatures=2000, fastThreshold=12, nlevels=8, scaleFactor=1.2, patchSize=31)
 matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
 templatesF = build_templatesF(templates, orb)
 
