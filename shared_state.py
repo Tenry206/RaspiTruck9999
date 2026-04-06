ed0f11600dee48ca486e9ae5b073b2f9cec07d45
Tenry <tenrytan55gmail.com>
2026-04-06 07:51:25 +0100

    Threading 1st try

---
+++ shared_state.py
@@ -0,0 +1,34 @@
+import threading
+
+class SharedState:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.running = True
+        self.frame = None
+        self.steering = (0,0)
+        self.override = None #for action like STOP or SPIN safely
+    
+    def update_frame(self, frame):
+        with self.lock:
+            self.frame = frame
+    
+    def get_frame(self):
+        with self.lock:
+            return self.frame
+        
+    def set_steering(self, left, right):
+        with self.lock:
+            self.steering = (left, right)
+
+    def get_steering(self):
+        with self.lock:
+            return self.steering
+    
+    def set_override(self, action):
+        with self.lock:
+            self.override = action
+    
+    def get_override(self):
+        with self.lock:
+            return self.override
+        
