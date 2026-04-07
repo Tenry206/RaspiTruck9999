5e869a789c9197e8c75313db6c0d8909cf701b82
LloydTTG <randyngui08@gmail.com>
2026-04-07 21:57:28 +0800

    recycle

--- recycle.py
+++ recycle.py
@@ -260,7 +260,8 @@ def thread_vision():
             continue
 
         h, w = frame.shape[:2]
-        vision_roi = frame[int(h * 0.0):int(h * 0.8),int(w * 0.15):int(w * 0.85), :]
+        vision_roi = frame[int(h * 0.0):int(h * 0.7),int(w * 0.1):int(w * 0.9), :]
+        vision_roi = cv2.resize(vision_roi, None, fx = 0.5, fy = 0.5)
         frame_gray = cv2.cvtColor(vision_roi, cv2.COLOR_BGR2GRAY)
         detected_shapes, shape_thresh = process_shapes(vision_roi)
 
