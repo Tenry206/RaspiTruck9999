1087d1e058ad372581e8697670bb046c2a55fb11
LloydTTG <randyngui08@gmail.com>
2026-04-08 12:43:08 +0800

    colored line implementation

--- recycle.py
+++ recycle.py
@@ -386,7 +386,7 @@ print("Initializing System ...")
 
 cam = Camera(resolution=(640,480), fps=30)
 coloredLine = toilet()
-orb = cv2.ORB_create(nfeatures=1000, fastThreshold=25, nlevels=12, scaleFactor=1.2, patchSize=31) #(nfeatures=1800, fastThreshold=14, nlevels=12, scaleFactor=1.2, patchSize=31)
+orb = cv2.ORB_create(nfeatures=1800, fastThreshold=15, nlevels=12, scaleFactor=1.2, patchSize=31) #(nfeatures=1800, fastThreshold=14, nlevels=12, scaleFactor=1.2, patchSize=31)
 
 matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
 templatesF = build_templatesF(templates, orb)
