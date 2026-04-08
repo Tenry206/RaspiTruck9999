ac7d74aac349ec96906ea853d73b5a180faf33d1
LloydTTG <randyngui08@gmail.com>
2026-04-08 11:01:10 +0800

    no more shape face

--- recycle.py
+++ recycle.py
@@ -328,7 +328,7 @@ def thread_vision():
         sleep(0.01)
 
         fps = 1.0 / (time() - start)
-        #print(fps)
+        print(fps)
 
 def thread_motor():
     while state.running:
@@ -369,7 +369,7 @@ print("Initializing System ...")
 
 cam = Camera(resolution=(640,480), fps=60)
 coloredLine = toilet()
-orb = cv2.ORB_create(nfeatures=500, fastThreshold=30, nlevels=12, scaleFactor=1.2, patchSize=31) #(nfeatures=1800, fastThreshold=14, nlevels=12, scaleFactor=1.2, patchSize=31)
+orb = cv2.ORB_create(nfeatures=3000, fastThreshold=40, nlevels=12, scaleFactor=1.2, patchSize=31) #(nfeatures=1800, fastThreshold=14, nlevels=12, scaleFactor=1.2, patchSize=31)
 
 matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
 templatesF = build_templatesF(templates, orb)
