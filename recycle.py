92abaea34d9a483352ede4351aaf5e9aea5acddb
LloydTTG <randyngui08@gmail.com>
2026-04-08 12:02:38 +0800

    colored line implementation

--- recycle.py
+++ recycle.py
@@ -308,7 +308,7 @@ def thread_vision():
 
                 orb_delay = (time() - orb_start_time) *1000
                 #print(f"WARNING: ORB Stalled motor for {orb_delay:.1f} ms!")
-                print(symbol)
+                print(f"Detected Symbol: {symbol}")
                 if symbol !=None:
                     if symbol == 'fingerprint' or symbol == 'qr':
                         #state.set_override('FACE_SCAN')
@@ -363,9 +363,9 @@ def thread_motor():
 
 print("Initializing System ...")
 
-cam = Camera(resolution=(640,480), fps=30)
+cam = Camera(resolution=(640,480), fps=60)
 coloredLine = toilet()
-orb = cv2.ORB_create(nfeatures=500, fastThreshold=38, nlevels=12, scaleFactor=1.2, patchSize=31) #(nfeatures=1800, fastThreshold=14, nlevels=12, scaleFactor=1.2, patchSize=31)
+orb = cv2.ORB_create(nfeatures=500, fastThreshold=40, nlevels=12, scaleFactor=1.2, patchSize=31) #(nfeatures=1800, fastThreshold=14, nlevels=12, scaleFactor=1.2, patchSize=31)
 
 matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
 templatesF = build_templatesF(templates, orb)
