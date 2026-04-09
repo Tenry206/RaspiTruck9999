a1fc49dac32ac0f7c24bddbf226cbc33d316c198
LloydTTG <randyngui08@gmail.com>
2026-04-09 12:00:11 +0800

    stop at shapes

--- hermawan.py
+++ hermawan.py
@@ -19,7 +19,7 @@ if not hasattr(state, 'current_shape'):
 # ------ Motor A GPIO setup ------
 
 
-ENA = pwm(17, frequency=400)
+ENA = pwm(17, frequency=600)
 IN1 = OutputDevice(22)
 IN2 = OutputDevice(27)
 
@@ -27,7 +27,7 @@ IN2 = OutputDevice(27)
 # ------ Motor B GPIO setup ------
 
 
-ENB = pwm(13, frequency=400)
+ENB = pwm(13, frequency=600)
 IN3 = OutputDevice(5)
 IN4 = OutputDevice(6)
     
